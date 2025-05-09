ebdb5b408a44dfed9c5b8425935ccdfa5e9e8934
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-09 16:20:33 -0400

    GET serverless route

---
+++ api/getFelines.js
@@ -0,0 +1,32 @@
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+export default async function handler(req, res){
+    //CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if(req.method === 'OPTIONS'){
+        return res.status(200).end();
+    }
+
+    if (req.method !== 'GET'){
+        return res.status(405).json({error: 'Method not allowed'});
+    }
+
+    try{
+        await client.connect();
+
+        let photosCollection = client.db('PepePics').collection('pictureData');
+        let photoData = await photosCollection.find().toArray();
+        res.status(200).json(photoData);
+    }
+    catch(error){
+        console.error(error)
+    }
+    finally{
+        await client.close();
+    }
+}

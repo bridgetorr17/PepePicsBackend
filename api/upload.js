c598ead0fd233741972f4e3ac14cf6ff2d55c852
bridgetorr17 <bridgetorr1902@gmail.com>
2025-05-21 18:20:40 -0400

    CORS handling for POST

--- api/upload.js
+++ api/upload.js
@@ -27,7 +27,7 @@ const pictureData = {
 //serverless function handler
 export default async function handler(req, res) {
     //CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://bridgetorr17.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
